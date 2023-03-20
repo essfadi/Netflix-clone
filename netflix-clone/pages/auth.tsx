00372bc0272eaa330d251c6ab346804d4875b3db
essfadi <o.essfadi@aui.ma>
2023-03-20 03:07:03 +0000

    feat: Navbar completed

--- netflix-clone/pages/auth.tsx
+++ netflix-clone/pages/auth.tsx
@@ -1,5 +1,5 @@
 "use client"
-import Input from "@/components/input";
+import Input from "@/components/Input";
 import React from "react";
 import axios from "axios";
 import { signIn } from 'next-auth/react';
